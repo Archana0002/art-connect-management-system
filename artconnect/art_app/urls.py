4cd764d56eaf8ae518fd7c9a7dcf1529cd5e3ddc
Archana0002 <archanaprakasan02@gmail.com>
2025-02-21 15:19:32 +0530

    Added art upload

--- artconnect/art_app/urls.py
+++ artconnect/art_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("details",views.details_page,name="details"),
     path("artist",views.artist_page,name="artist"),
-    path("artist_profile",views.artist_profile_page,name="artist_profile")
+    path("artist_profile",views.artist_profile_page,name="artist_profile"),
+    path("art_upload",views.art_upload,name="art_upload")
 ]
