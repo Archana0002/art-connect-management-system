beb828dc1e7893b8c2380a4df8b508c5e9e51ff2
Renjish-k-s <renjishksimon@gmail.com>
2025-03-26 16:56:04 +0530

    scrapping completed update

--- artconnect/scraper/urls.py
+++ artconnect/scraper/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('events/<int:city_id>/', views.scrape_events, name="scrape_events"),
     path('sclupture/', views.sclupture, name="sclupture_show"),    
+    path('painting/', views.painting, name="painting"),    
+
 ]
